1cd33757d10573a6b9dc34f656a137130ecabb37
Vladimir Korobov <voir703@gmail.com>
2022-11-22 16:47:25 +0500

    add task5

---
+++ task5/Program.cs
@@ -0,0 +1,15 @@
+﻿/* Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа.
+456 -> 6
+782 -> 2
+918 -> 8
+*/
+
+int number = Int32.Parse(Console.ReadLine());
+if (number > 999 && number < 100)
+{
+    Console.WriteLine("wrong number");
+return;
+}    
+int number2 = number % 10;  // если днаписать % 100 то покажет два последних числа
+Console.WriteLine(number2);
+
