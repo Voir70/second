e564f498abb4a8dd6d0a95ae6b974fd235f59838
Vladimir Korobov <voir703@gmail.com>
2022-11-22 16:12:52 +0500

    add task4

--- task3/Program.cs
+++ task3/Program.cs
@@ -8,26 +8,33 @@ Console.WriteLine("Введите день недели");
 string numberStr = Console.ReadLine();
 int number = Convert.ToInt32(numberStr);
 
-if(number == 1)
+if (number == 1)
 {
-Console.WriteLine("Понедельник");
-} else if (number == 2){
-  Console.WriteLine("Вторник");  
+    Console.WriteLine("Понедельник");
 }
-else if (number == 3){
-  Console.WriteLine("Среда");  
+else if (number == 2)
+{
+    Console.WriteLine("Вторник");
 }
-else if (number == 4){
-  Console.WriteLine("Четверг");  
+else if (number == 3)
+{
+    Console.WriteLine("Среда");
 }
-else if (number == 5){
-  Console.WriteLine("Пятница");  
+else if (number == 4)
+{
+    Console.WriteLine("Четверг");
 }
-else if (number == 6){
-  Console.WriteLine("Суббота");  
+else if (number == 5)
+{
+    Console.WriteLine("Пятница");
 }
-else if (number == 7){
-  Console.WriteLine("Воскресение");  
+else if (number == 6)
+{
+    Console.WriteLine("Суббота");
 }
-else 
-Console.WriteLine("Ввели не число недели от 1 до 7");
+else if (number == 7)
+{
+    Console.WriteLine("Воскресение");
+}
+else
+    Console.WriteLine("Ввели не число недели от 1 до 7");
---
+++ task4/Program.cs
@@ -0,0 +1,11 @@
+﻿/*Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до 
+4 -> " -4, -3, -2, -1, 0, 1, 2, 3, 4"
+2 -> " -2, -1, 0, 1, 2" */
+
+Console.WriteLine("Введите число ");
+string numberFirst = Console.ReadLine();      // а можно две строчки в одной уместить string numberFirst = Convert.ToInt32(Console.ReadLine());
+int number = Convert.ToInt32(numberFirst);
+
+number = Math.Abs(number);
+for (int i = -number; i <= number; i++)
+    Console.Write($"{i},");
