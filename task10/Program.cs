260b30c7d9840c59359cb88fc392825b6b955124
Vladimir Korobov <Voir703@gmail.com>
2022-11-24 21:09:15 +0300

    add task10

---
+++ task10/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Write number 1");
+int number = int.Parse(Console.ReadLine());
+Console.WriteLine("Write number 2");
+int number_2 = int.Parse(Console.ReadLine());
+if (number*number==number_2 || number_2*number_2==number)
+{
+    Console.Write("Yes");
+}
+else 
+{
+    Console.Write("No");
+}
