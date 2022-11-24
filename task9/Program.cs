67ed06327753c5f88bbdfcaca1b5fa0914448359
Vladimir Korobov <Voir703@gmail.com>
2022-11-24 20:51:59 +0300

    add task9

---
+++ task9/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Введите число");
+int number = int.Parse(Console.ReadLine());
+if (number % 7 == 0 && number % 23 == 0){
+    Console.Write("Yes");
+}
+else 
+    Console.Write("no");
