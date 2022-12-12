2351af0bfd876d8e12f7da203022a5cf924c8352
Vladimir Korobov <Voir703@gmail.com>
2022-12-12 22:12:02 +0300

    add task 24

---
+++ task24/Program.cs
@@ -0,0 +1,39 @@
+﻿/*Задача 51: Задайте двумерный массив. Найдите сумму элементов, находящихся на главной диагонали (с индексами (0,0); (1;1) и т.д.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+Сумма элементов главной диагонали: 1+9+2 = 12
+*/
+// 1. функция ввода числа
+int GetNumber(string text)
+{
+    int result = 0;
+    Console.WriteLine(text);
+    while(true)
+    {
+        if (int.TryParse(Console.ReadLine(), out result))
+        {
+            break;
+        }
+    }
+    return result;
+}
+
+// 2. функция задачи рандомного массива
+int[,] GetArray(int m, int n)
+{
+    int [,] array = new int [m, n];
+    Random rnd = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rnd.Next(0,11);
+        }
+    }
+    return array;
+}
+
+
+
