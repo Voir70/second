315391c7883a8027523ffda9a1e7a4bf8e0341b8
Vladimir Korobov <Voir703@gmail.com>
2022-12-12 20:28:54 +0300

    add task 21

---
+++ task21/Program.cs
@@ -0,0 +1,47 @@
+﻿/*Задача 46: Задайте двумерный массив размером m×n, заполненный случайными целыми числами.
+m = 3, n = 4.
+1 4 8 19
+5 -2 33 -2
+77 3 8 1
+*/
+//получить числа с консоли
+int GetNumber(string message)
+{
+    int result = 0;
+
+    while(true)
+    {
+        Console.WriteLine(message);
+        if(int.TryParse(Console.ReadLine(), out result))
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не число");
+        }
+    }
+
+    return result;
+}
+
+//задать матрицу и заполнить ее числами
+//распечатать матрицу
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int m = GetNumber("Введите количество строк:");
+int n = GetNumber("Введите количество столбцов");
+
+int[,] matrix = GetMatrix(1,10);
+
+PrintMatrix(matrix);
