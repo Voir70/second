5eef9db64b3733598e6e9c9cf0000497081b7f12
Vladimir Korobov <voir703@gmail.com>
2022-12-16 09:17:49 +0500

    add tasr 25

--- task25/Program.cs
+++ task25/Program.cs
@@ -1,24 +1,7 @@
 ﻿/*Задача 53: Задайте двумерный массив. 
 Напишите программу, которая поменяет местами первую и последнюю строку массива.
 */
-//функция получения положительного числа
-int GetPositiveNumber(string text)
-{
-    int result = 0;
-    Console.WriteLine(text);
-    while(true)
-    {
-        if (int.TryParse(Console.ReadLine(), out result) && result > 0)
-        {
-            break;
-        }
-        else
-        {
-            Console.WriteLine("Ввели число меньше 1");
-        }
-    }
-    return result;
-}
+
 //функция получения числа
 int GetNumber(string text)
 {
@@ -37,54 +20,7 @@ int GetNumber(string text)
     }
     return result;
 }
-//функция замены строк 
-//функция печати 
-//функция получения положительного числа
-int GetPositiveNumber(string text)
-{
-    int result = 0;
-    Console.WriteLine(text);
-    while(true)
-    {
-        if (int.TryParse(Console.ReadLine(), out result) && result > 0)
-        {
-            break;
-        }
-        else
-        {
-            Console.WriteLine("Ввели число меньше 1");
-        }
-    }
-    return result;
-}
-//функция получения числа
-int GetNumber(string text)
-{
-    int result = 0;
-    Console.WriteLine(text);
-    while(true)
-    {
-        if (int.TryParse(Console.ReadLine(), out result))
-        {
-            break;
-        }
-        else
-        {
-            Console.WriteLine("Ввели не число");
-        }
-    }
-    return result;
-}
-int m = GetPositiveNumber("Введите m");
-int n = GetPositiveNumber("Введите n");
-int minValue = GetNumber("Введите минимальное значение массива");
-int maxValue = GetNumber("Введите максимальное значение массива");
-int[,] array = GetArray(m, n, minValue, maxValue);
-PrintArray(array);
-int[,] resultArray = ArrayStringReplace(array);
-Console.WriteLine();
-PrintArray(resultArray);
-От Yuriy всем 08:51 PM
+
 //функция получения массива
 int[,] GetArray(int m, int n, int minValue, int maxValue)
 {
@@ -100,3 +36,70 @@ int[,] GetArray(int m, int n, int minValue, int maxValue)
     return array;
 }
 
+//функция замены строк массива
+int[,] ArrayStringReplace(int[,] array)
+{
+    int[,] resultArray = new int[array.GetLength(0), array.GetLength(1)];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (i == 0)
+            {
+                resultArray[i, j] = array[array.GetLength(0) - 1, j];
+            }
+            
+
+             else if (i == array.GetLength(0) - 1)
+            {
+                resultArray[i, j] = array[0, j];
+            }
+            else
+            {
+                resultArray[i, j] = array[i, j];
+            }
+        }
+    }
+    return resultArray;
+}
+//функция получения положительного числа
+int GetPositiveNumber(string text)
+{
+    int result = 0;
+    Console.WriteLine(text);
+    while (true)
+    {
+        if (int.TryParse(Console.ReadLine(), out result) && result > 0)
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели число меньше 1");
+        }
+    }
+    return result;
+}
+
+//функция печати массива
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int m = GetPositiveNumber("Введите m");
+int n = GetPositiveNumber("Введите n");
+int minValue = GetNumber("Введите минимальное значение массива");
+int maxValue = GetNumber("Введите максимальное значение массива");
+int[,] array = GetArray(m, n, minValue, maxValue);
+PrintArray(array);
+int[,] resultArray = ArrayStringReplace(array);
+Console.WriteLine();
+PrintArray(resultArray);
